0ab85760c4039c31cf2f71865c9d4f8713bc363f
sanketahegde <sanketahegde@gmail.com>
2023-05-12 16:03:46 +0200

    speed-up the lstm model inference using onnx

--- modules/LSTM_predict.py
+++ modules/LSTM_predict.py
@@ -1,10 +1,8 @@
-import os
+import numpy
 import tensorflow as tf
-from scipy.signal import find_peaks
 from tensorflow import keras
 import numpy as np
-from matplotlib import pyplot as plt
-import plotly.express as px
+import onnxruntime as ort
 
 
 def normalize(array):
@@ -17,7 +15,7 @@ def get_model_input_data(data):
     offset = 1000
     data_bins = []
 
-    for i in range(0, len(data), 1000):
+    for i in range(len(data) // offset + 1):
         samples = data[start:end]
         start += offset
         end += offset
@@ -37,17 +35,14 @@ def get_model_input_data(data):
 
 
 def detect_r_peaks(data):
-    lstm_model = keras.models.load_model('models/lstm_model')
-    model_input = get_model_input_data(data)
-    model_output = lstm_model.predict(model_input)
+    # use the tensorflow to onnx converted lstm model for predictions
+    sess = ort.InferenceSession('models/lstm_model.onnx')
+    input_name = sess.get_inputs()[0].name
+    output_name = sess.get_outputs()[0].name
+    model_input = get_model_input_data(data).astype(numpy.float32)
+    model_output = sess.run([output_name], {input_name: model_input})
     model_output = np.reshape(model_output, (-1))
     full_lstm_pred_array = model_output[:len(data)]
-    # full_lstm_pred_array[full_lstm_pred_array >= 0.3] = 1
-    # full_lstm_pred_array[full_lstm_pred_array < 0.3] = 0
-    r_peaks = find_peaks(full_lstm_pred_array, height=0.3)
-    # plt.rcParams["figure.figsize"] = (20, 6)
-    # time = np.arange(0, len(data) / 511, 1 / 511)
-    # plt.plot(time[:len(data)], data)
-    # plt.scatter(x=r_peaks[0]/511, y=data[r_peaks[0]], marker='o', c='red')
-    # plt.show()
+    full_lstm_pred_array = np.where(full_lstm_pred_array >= 0.3, 1, 0)
+    r_peaks = np.where(full_lstm_pred_array == 1)
     return r_peaks[0]
